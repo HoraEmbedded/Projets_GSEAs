70e7ee7bfccf9af768119af4dfba04fa083698fa
HoraEmbeddded <azonhoumonhoracia@gmail.com>
2026-06-06 16:16:09 +0100

    Update fsm(transition).c
    feat: add 30s timeout and UART message to SELECTION state

--- 01_Info_Embarquee/FSM_Making/fsm(transition).c
+++ 01_Info_Embarquee/FSM_Making/fsm(transition).c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <stdint.h> // Permet d'utiliser des types entiers précis comme uint32_t
+
+
+uint32_t GetSystemTick(void) {
+    
+    return 0; 
+}
+
+//(Message UART)
+void SendUartTimeoutMessage(void) {
+    // En vrai C embarqué, on utiliserait une fonction comme HAL_UART_Transmit()
+    printf("UART MESSAGE : Timeout detecte, retour a l'accueil.\n");
+}
+
+
 // 0. On définit les types de l'état et de l'événement pour la machine
 typedef enum{
     IDLE,
@@ -37,7 +53,7 @@ void PaymentBoard(void) { printf("Action : Attente paiement\n"); }
 void DispenseDrink(void) { printf("Action : Boisson servie\n"); }
 void ReturnMoney(void) { printf("Action : Rendu monnaie\n"); }
 
-// 4. La fameuse Table de Transitions (Notre fichier Excel)
+// 4. Table de Transitions
 StateTransition fsm_table[] = {
     // ETAT ACTUEL, EVENEMENT,    PROCHAIN ETAT,  ACTION
     {IDLE,          PRES_BTN,     SELECTION,      DisplayMenu},
@@ -50,10 +66,30 @@ StateTransition fsm_table[] = {
 // calcul automatique de la taille du tableau pour l'ordinateur
 #define TABLE_SIZE (sizeof(fsm_table)/sizeof(fsm_table[0]))
 
-void run_fsm(void){
-
+void run_fsm() {
     MachineState currentState = IDLE;
     MachineEvent currentEvent = NO_EVENT;
+    
+    //  heure entré dans l'état SELECTION
+    uint32_t time_entered_selection = 0; 
+
+    while(1) {
+        // A.  vérifie les capteurs
+        currentEvent = read_sensor(); 
+
+        // B. GESTION DU TEMPS (La nouveauté !)
+        // Si on est dans l'état SELECTION et qu'aucun bouton n'a été touché...
+        if (currentState == SELECTION && currentEvent == NO_EVENT) {
+            
+            // On calcule la différence entre "Maintenant" et "Le moment où on est entré"
+            uint32_t time_elapsed = GetSystemTick() - time_entered_selection;
+            
+            // 30 secondes = 30 000 millisecondes
+            if (time_elapsed >= 30000) {
+                currentEvent = TIMEOUT30S; // On génère artificiellement l'événement !
+                SendUartTimeoutMessage();  // On envoie le message UART demandé
+            }
+        }
 
     while(1) {
 
