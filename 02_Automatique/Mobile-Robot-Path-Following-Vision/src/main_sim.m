9f10a628139508f24760499b73902697eb8fd331
HoraEmbeddded <azonhoumonhoracia@gmail.com>
2026-06-21 21:02:37 +0100

    Update main_sim.m
    feat: Add environment loading and implement distance sensor in Simulink via MATLAB Function

--- 02_Automatique/Mobile-Robot-Path-Following-Vision/src/main_sim.m
+++ 02_Automatique/Mobile-Robot-Path-Following-Vision/src/main_sim.m
@@ -15,6 +15,21 @@ run('waypoint.m');
 lookahead_dist = 20; 
 desired_vel = 50; 
 
+disp('Loading environment map...');
+load('../data/environment_map.mat');
+
+% On recalcule les mêmes indices que dans create_environment.m
+num_obstacles = 2; 
+intervals = linspace(0.2, 0.8, num_obstacles);
+obstacles_pos = zeros(num_obstacles, 2); % Matrice [N x 2] pour stocker (X,Y)
+
+for i = 1:num_obstacles
+    idx = round(intervals(i) * length(waypoints));
+    obstacles_pos(i, 1) = waypoints(idx, 1); % X de l'obstacle i
+    obstacles_pos(i, 2) = waypoints(idx, 2); % Y de l'obstacle i
+end
+
+
 %% 3. Run Simulink Model
 disp('Running Simulink model...');
 % Increased StopTime to allow the robot to complete the path
